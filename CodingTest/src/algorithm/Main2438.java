3dc6f887afedea9f8fb8f7bd886dc5c614ee1b69
mangtaeeee <osp9658@naver.com>
2022-06-24 17:16:40 +0900

    22.06.24

--- CodingTest/src/algorithm/Main2438.java
+++ CodingTest/src/algorithm/Main2438.java
@@ -10,6 +10,7 @@ public class Main2438 {
 
 	//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader sr = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(sr.readLine());
 		StringBuilder sb = new StringBuilder();
