001eff2f21b29248ce47235928b51d462e609e3a
mangtaeeee <osp9658@naver.com>
2022-07-05 17:10:32 +0900

    22.07.05

--- CodingTest/src/algorithm/Main2439.java
+++ CodingTest/src/algorithm/Main2439.java
@@ -1,10 +1,27 @@
 package algorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main2439 {
-	public static void main(String[] args) {
-		/**첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-		하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.*/
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				sb.append(' ');
+			}
+			for (int k = 1; k <= i; k++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb);
 	}
 
 }
