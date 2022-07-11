20e4c9804af0971b56118f177e1fb874478e9cde
mangtaeeee <osp9658@naver.com>
2022-07-11 11:12:44 +0900

    22.07.11

---
+++ CodingTest/src/algorithm/Main10871.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Main10871 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- CodingTest/src/algorithm/Main2741.java
+++ CodingTest/src/algorithm/Main2741.java
@@ -11,7 +11,7 @@ public class Main2741 {
 		for (int i = 1; i <= n ; i++) {
 			System.out.println(i);
 		}
-
+		sc.close();
 	}
 
 }
