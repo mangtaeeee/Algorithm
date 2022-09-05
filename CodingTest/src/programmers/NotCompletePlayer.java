572cfb97b3067ef9822a941e5765e0a04d7e3f54
mangtaeeee <osp9658@naver.com>
2022-09-05 13:39:43 +0900

    [22.09.05]

--- CodingTest/src/programmers/NotCompletePlayer.java
+++ CodingTest/src/programmers/NotCompletePlayer.java
@@ -8,7 +8,7 @@ public class NotCompletePlayer {
 		//두개의 배열을 정렬 후
 		Arrays.sort(participant);
 		Arrays.sort(completion);
-		//return을 위한 변수 선언
+		//return을 위한 변수 선언 또한 마지막에 선수가 다른값일수 있으므로 선언
 		int i = 0;
 		//반복문을 통한 비교 두배열이 다를때까지 찾는다.
 		for(i = 0 ; i < completion.length; i++) {
