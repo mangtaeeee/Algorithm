0ac17c4a72fd80d97bb3e0d14cfca3cf7d292f6a
mangtaeeee <osp9658@naver.com>
2024-08-21 21:27:39 +0900

    [24.08.21] 자기전 뇌풀이

---
+++ CodingTest/src/algorithm/Main25314.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main25314 {
+	public static void main(String[]args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = n/4;
+		for (int i = 0; i < m ; i++) {
+			System.out.println("long ");
+		}
+		System.out.println("int");
+	}
+
+}
