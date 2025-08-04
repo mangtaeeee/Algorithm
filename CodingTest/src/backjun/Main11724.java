67ecbd6e6569d08756521dfd48a81940b11fc012
mangtaeeee <osp9658@naver.com>
2025-08-04 19:03:39 +0900

    25.08.04 dfs 2문제

---
+++ CodingTest/src/backjun/Main11724.java
@@ -0,0 +1,71 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
+
+첫째 줄에 정점의 개수 N과 간선의 개수 M이 주어진다.
+(1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다.
+(1 ≤ u, v ≤ N, u ≠ v) 같은 간선은 한 번만 주어진다.
+
+첫째 줄에 연결 요소의 개수를 출력한다.
+
+* */
+public class Main11724 {
+
+    static int N, M;                           // 정점 개수, 간선 개수
+    static List<Integer>[] graph;                   // 인접 리스트
+    static boolean[] visited;                  // 방문 체크
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        //그래프 초기화
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        //간선 정보 입력
+        for (int i = 0 ; i < M ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        visited = new boolean[N + 1];
+        int count = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        // 결과 출력
+        System.out.println(count);
+
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
