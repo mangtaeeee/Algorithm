16b32f9e28f55e6d4dc9afffc2059c9dc74d33dc
mangtaeeee <osp9658@naver.com>
2025-11-11 13:29:15 +0900

    25.11.11 17일 동안 코테 연습하기 Day3 첫번째 문제

---
+++ CodingTest/src/backjun/day17/Solution42586.java
@@ -0,0 +1,51 @@
+package backjun.day17;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+//또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고,
+// 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+//먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와
+// 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 각 배포마다 몇
+// 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+//  progresses	  speeds	return
+// [93, 30, 55]	[1, 30, 5]	[2, 1]
+public class Solution42586 {
+
+    public static void main(String[] args) {
+        Solution42586 solution42586 = new Solution42586();
+        int[] a = new int[]{93, 30, 55};
+        int[] b = new int[]{1, 30, 5};
+
+        ArrayList<Integer> solution = solution42586.solution(a, b);
+        System.out.println("solution = " + solution);
+    }
+
+    public ArrayList<Integer>  solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i =0; i < progresses.length ; i ++) {
+            queue.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
+        }
+
+        int count = 1;
+        int first = queue.poll();
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            if (first >= current) {
+                count ++;
+            } else {
+                answer.add(count);
+                first = current;
+                count = 1;
+            }
+        }
+        answer.add(count);
+
+        return answer;
+    }
+}
