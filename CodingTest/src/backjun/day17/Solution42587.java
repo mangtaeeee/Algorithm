7352e91acbe75fc2dafba91fc44b4292c3bc877f
mangtaeeee <osp9658@naver.com>
2025-11-11 14:37:18 +0900

    25.11.11 17일 동안 코테 연습하기 Day3 두번째 문제

---
+++ CodingTest/src/backjun/day17/Solution42587.java
@@ -0,0 +1,38 @@
+package backjun.day17;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Solution42587 {
+
+    public static void main(String[] args) {
+        Solution42587 solution42587 = new Solution42587();
+        int[] arr = new int[] {1, 1, 9, 1, 1, 1};
+        int solution = solution42587.solution(arr, 0);
+        System.out.println("solution = " + solution);
+    }
+    public int solution(int[] priorities, int location) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int priority : priorities) {
+            pq.add(priority);
+        }
+        int answer = 0;
+
+        while (!pq.isEmpty()) {
+            for (int i = 0 ; i < priorities.length; i++) {
+                if (pq.peek() == priorities[i]) {
+                    pq.poll();
+                    answer++;
+                    if (location == i) {
+                        return answer;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
